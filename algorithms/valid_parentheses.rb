e6c2680064f21c8b535cdf183e7c4b0430245a38
github username <github email address>
2019-10-08 19:45:42 -0400

    valid_parentheses

---
+++ algorithms/valid_parentheses.rb
@@ -0,0 +1,15 @@
+def is_valid(s)
+    return true if s.empty?
+    stack = []
+    brackets = {")" => "(", "]" => "[", "}" => "{"}
+    s.split('').each do |char|
+        if brackets.values.include?(char)
+            stack << char
+        else
+            return false if stack.pop != brackets[char]
+        end
+    end
+    stack.empty?
+
+
+ end
